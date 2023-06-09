bbfb8999fa248bb49c3e45ebc81f9827b1edf874
lesinai <90961075+lesinai@users.noreply.github.com>
2023-06-09 20:12:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 //import { User } from './User/User';
-import statistic from '../data/statistic.json';
+import statistic from '../data/data.json';
 import user from '../data/user.json';
 import friends from '../data/friends.json';
 import transactions from '../data/transactions.json';
