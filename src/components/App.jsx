ef1ff14a265440286ed260ce709a295e12a76e1b
lesinai <90961075+lesinai@users.noreply.github.com>
2023-06-09 19:17:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,16 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-//import { User } from './User/User';
-import statistic from '../data/statistic.json';
-import user from '../data/user.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
-
 export const App = () => {
   return (
     <div
-      className="app"
       style={{
-        minHeight: '100vh',
+        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101',
+        color: '#010101'
       }}
     >
-      <Profile user={user} />
-      <Statistics stats={statistic} title="Upload stats" />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      React homework template
     </div>
   );
 };
